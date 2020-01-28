eaa89c93f11bb2649af7b94fc0f23925d2f673f1
facebotgit <59485152+facebotgit@users.noreply.github.com>
2020-01-28 18:50:01 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,47 @@
+function detectmob() {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function LoadCSS( cssURL ) {
+
+    // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
+
+    return new Promise( function( resolve, reject ) {
+
+        var link = document.createElement( 'link' );
+
+        link.rel  = 'stylesheet';
+
+        link.href = cssURL;
+
+        document.head.appendChild( link );
+
+        link.onload = function() { 
+
+            resolve(); 
+
+            console.log( 'CSS has loaded!' ); 
+        };
+    } );
+}
+
+if (!detectmob())
+{
+    LoadCSS('./css/desktop.css');
+}
+else
+{
+    LoadCSS('./css/mobile.css');
+}
---
+++ src/slideshow.js
@@ -0,0 +1,18 @@
+// const img = document.querySelector('img[slideshow=false]');
+// img.setAttribute('slideshow', 'true');
+
+// var firstImg = true;
+
+// // function changeImg()
+// // {
+// //     if (firstImg)
+// //     img.setAttribute('src', './img/fbt2.png');
+// // else
+// //     img.setAttribute('src', './img/fbt1.png');
+
+// //     firstImg = !firstImg;
+// // }
+
+// // window.setInterval(() => {
+// //     changeImg();
+// // }, 5000);
