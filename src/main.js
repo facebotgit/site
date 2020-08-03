924fb69d9be346d37a6821d9b986cdf4a50e0c20
facebotgit <59485152+facebotgit@users.noreply.github.com>
2020-08-03 17:37:38 -0300

    Novo site
    Novo site, novo layout

--- src/main.js
+++ src/main.js
@@ -1,47 +1,47 @@
-function detectmob() {
-    if (navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)
-    ) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+// function detectmob() {
+//     if (navigator.userAgent.match(/Android/i)
+//         || navigator.userAgent.match(/webOS/i)
+//         || navigator.userAgent.match(/iPhone/i)
+//         || navigator.userAgent.match(/iPad/i)
+//         || navigator.userAgent.match(/iPod/i)
+//         || navigator.userAgent.match(/BlackBerry/i)
+//         || navigator.userAgent.match(/Windows Phone/i)
+//     ) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
 
-function LoadCSS( cssURL ) {
+// function LoadCSS( cssURL ) {
 
-    // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
+//     // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
 
-    return new Promise( function( resolve, reject ) {
+//     return new Promise( function( resolve, reject ) {
 
-        var link = document.createElement( 'link' );
+//         var link = document.createElement( 'link' );
 
-        link.rel  = 'stylesheet';
+//         link.rel  = 'stylesheet';
 
-        link.href = cssURL;
+//         link.href = cssURL;
 
-        document.head.appendChild( link );
+//         document.head.appendChild( link );
 
-        link.onload = function() { 
+//         link.onload = function() { 
 
-            resolve(); 
+//             resolve(); 
 
-            console.log( 'CSS has loaded!' ); 
-        };
-    } );
-}
+//             console.log( 'CSS has loaded!' ); 
+//         };
+//     } );
+// }
 
-if (!detectmob())
-{
-    LoadCSS('./css/desktop.css');
-}
-else
-{
-    LoadCSS('./css/mobile.css');
-}
+// if (!detectmob())
+// {
+//     LoadCSS('./css/desktop.css');
+// }
+// else
+// {
+//     LoadCSS('./css/mobile.css');
+// }
